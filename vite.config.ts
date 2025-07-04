671996c646524d1a8c7418605bb5ae9e743b0ac7
Caio Rodrigues <tglcaiohenrique@gmail.com>
2025-07-04 15:21:21 -0300

    Atualização do caminho base para GitHub Pages // Atualização do tipo de dados da atividade

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Agrosimples/', // 👈 importante para GitHub Pages
+  base: '/Agrosimples/', // 👈 Caminho relativo para GitHub Pages
 });
